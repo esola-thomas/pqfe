a351ea8477a82c6d2b5bbd81507b3e7dda5e03db
Ernesto Sola-Thomas <93721212+esola-thomas@users.noreply.github.com>
2025-03-02 17:02:38 -0500

    Setup as python package & Fix docker  (#12)
    * Add setup.py for Post-Quantum File Encryption API
    
    * Update Dockerfile and add pyproject.toml for Post-Quantum File Encryption API
    
    * Refactor Dockerfile to streamline liboqs and liboqs-python installation process

---
+++ setup.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2025 Ernesto Sola-Thomas
+from setuptools import setup, find_packages
+
+setup(
+    name="pqfe",
+    version="0.1.0",
+    author="Ernesto Sola-Thomas",
+    author_email="ernesto@solathomas.com",
+    description="Post-Quantum File Encryption API with Kyber and hybrid encryption",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/esola-thomas/pqfe",
+    packages=find_packages(),
+    install_requires=[
+        "cryptography>=41.0.0",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10.16",
+        "Topic :: Security :: Cryptography"
+    ],
+    python_requires=">=3.8",
+)
